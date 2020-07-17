471e0baac82543b54485e153ba2816d84675d998
cyX_GUG <chenyangcyx@outlook.com>
2020-07-17 22:28:04 +0800

    set the program window to be centered at the beginning
    commit time: 2020/07/17 22:27:56

--- Complex Password Generator/NET Framwork Platform/UI/MainForm.Designer.cs
+++ Complex Password Generator/NET Framwork Platform/UI/MainForm.Designer.cs
@@ -531,8 +531,11 @@
             this.ClientSize = new System.Drawing.Size(878, 709);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Password Generator";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
